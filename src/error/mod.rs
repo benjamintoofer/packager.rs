c3da69cca12305f704ba1bbe4f8be01b986c904b
Benjamin Toofer <benjamintoofer@gmail.com>
2021-02-14 09:27:52 -0700

    feat(error): closes #6 Custom error with major and minor codes

--- src/error/mod.rs
+++ src/error/mod.rs
@@ -1,22 +1,40 @@
 use std::fmt;
 
-pub mod isobmff_errors;
+pub mod error_code;
 
-#[derive(Clone)]
-pub struct ParseUnsignedError {
+use error_code::{
+  MinorError,
+  MajorCode,
+};
+
+pub struct CustomError {
+  pub major: MajorCode,
+  pub minor: u8,
   pub message: String,
+  pub debug_message: String,
   pub file: &'static str,
   pub line: u32
 }
 
-impl fmt::Display for ParseUnsignedError {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        write!(f, "An error occurred attempting to parse and unsigned value")
-    }
+impl fmt::Display for CustomError {
+  fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+      write!(f, "{}", self.message)
+  }
 }
 
-impl fmt::Debug for ParseUnsignedError {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        write!(f, "{{ messsage: {}, file: {}, line: {} }}", self.message, self.file, self.line)
-    }
+impl fmt::Debug for CustomError {
+  fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+      write!(f, "{{ messsage: {}, file: {}, line: {} }}", self.debug_message, self.file, self.line)
+  }
+}
+
+pub fn construct_error(major: MajorCode, minor: Box<dyn MinorError>, debug_message: String, file: &'static str, line: u32) -> CustomError {
+  CustomError{
+    major,
+    minor: minor.code(),
+    message: minor.message(),
+    debug_message,
+    file,
+    line
+  }
 }
