31ea0e74ecb18fadfd3cf87b775b4457de9a69ab
Benjamin Toofer <benjamintoofer@gmail.com>
2021-02-27 18:06:43 -0700

    feat(transcode): close #23 Add transcoding and fragmenting with ffmpeg and bento

---
+++ src/transcoder/ffmpeg.rs
@@ -0,0 +1,50 @@
+use std::process::Command;
+use std::thread;
+
+use super::VideoSize;
+
+pub struct FFMPEG {
+
+}
+
+impl FFMPEG {
+  pub fn transcode(input:&str, output: &str, sizes: Vec<VideoSize>) {
+    
+    let mut ffmpeg_command = Command::new("ffmpeg");
+    let mut args: Vec<String> = vec![
+      "-i".to_string(),
+      input.to_string()
+    ];
+    
+    for size in sizes {
+      args.append(&mut FFMPEG::args(size, output));
+    }
+
+    let handle  = thread::spawn(move || {
+      let mut child_process = ffmpeg_command
+        .args(args)
+        .spawn()
+        .expect("Failed to execute command");
+      
+      child_process.wait().unwrap();
+    });
+
+    handle.join().unwrap();
+  }
+
+  fn args(size: VideoSize, output: &str) -> Vec<String> {
+    println!("{}_{}.mp4", output, size.value());
+    vec![
+      "-y".to_string(), // Overrite existing files
+      "-c:v".to_string(), "libx264".to_string(), // Encode using libx264 encoder
+      "-x264opts".to_string(), "keyint=60:no-scenecut".to_string(), // x264 options, key interval every 60 frames
+      "-s".to_string(), size.value(),
+      "-r".to_string(), "30".to_string(),
+      "-preset".to_string(), "veryfast".to_string(),
+      "-c:a".to_string(), "aac".to_string(),
+      "-sws_flags".to_string(), "bilinear".to_string(),
+      format!("{}/{}.mp4", output, size.value())
+      // format!("./temp/output_{}.mp4",size.value())
+    ]
+  }
+}
